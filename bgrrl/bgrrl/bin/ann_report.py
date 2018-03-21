6658d7803a64776bacb9392060a766d3b6945014
Christian Schudoma (cschu) <christian.schudoma@earlham.ac.uk>
2018-03-21 19:47:51 +0000

    Corrected feature counts and added coordinate error annotation

--- bgrrl/bgrrl/bin/ann_report.py
+++ bgrrl/bgrrl/bin/ann_report.py
@@ -9,6 +9,8 @@ from collections import Counter, namedtuple, OrderedDict
 # FEATURES = ["gene", "CDS", "tRNA", "rRNA", "tmRNA", "ncRNA"]
 FEATURES = ["gene", "tRNA", "rRNA", "tmRNA", "ncRNA"]
 ANN_REPORT_HEADER = ["Reference"]
+ANN_REPORT_HEADER.extend(["Transfer coordinate issue?"])
+ANN_REPORT_HEADER.extend(["#Features[ref]", "#Features[transferred]", "Transferred[%]"])
 ANN_REPORT_HEADER.extend(map(lambda x:x+"[ref]", FEATURES))
 ANN_REPORT_HEADER.extend(map(lambda x:x+"[transferred]", FEATURES))
 ANN_REPORT_HEADER.extend(map(lambda x:x+"[%]", FEATURES))
@@ -21,6 +23,9 @@ GFFeature = namedtuple("GFFeature", "seqname source feature start end score stra
 
 def countGFFFeatures(_in):
     fcounter = dict((feature, set()) for feature in FEATURES)
+    genecounter = {None}
+    # genecounter = {None: set()}
+
     # return Counter(row[2] for row in csv.reader(_in, delimiter="\t") if row and not row[0].startswith("#"))
     for row in csv.reader(_in, delimiter="\t"):
         if row and not row[0].startswith("#"):
@@ -28,7 +33,15 @@ def countGFFFeatures(_in):
             ltag = attr.get("locus_tag", None)
             if ltag is not None and row[2] != "CDS":
                 fcounter.setdefault(row[2], set()).add(ltag)
-    return dict((k, len(fcounter[k])) for k in fcounter)
+                # genecounter.add(attr.get("ID", None))
+                genecounter.add(ltag)
+    
+    for k in fcounter:
+        if k != "gene":
+            fcounter["gene"].difference_update(fcounter[k])
+    genecounter.remove(None)
+
+    return dict((k, len(fcounter[k])) for k in fcounter), genecounter
             
         
  
@@ -48,13 +61,14 @@ def main(args_in=sys.argv[1:]):
     owalk = os.walk(args.ref_dir)
 
     refcounts = dict()
+    refgenecounts = dict()
 
     for d, dirs, files in os.walk(args.ref_dir):
         if os.path.basename(d) == "gff":
             for f in files:
                 if f.endswith(".gff"):
                     with open(os.path.join(d, f)) as fin:
-                        refcounts[f.strip(".gff")] = countGFFFeatures(fin)
+                        refcounts[f.strip(".gff")], refgenecounts[f.strip(".gff")] = countGFFFeatures(fin)
 
     print(refcounts.keys())
 
@@ -65,16 +79,23 @@ def main(args_in=sys.argv[1:]):
             header, rows = list(), list()
             for d in sorted(next(os.walk(sdir))[1]):
                 try:
+                    f = glob.glob(os.path.join(sdir, d, "*.final.gff.delta"))[0]
+                except:
+                    print("No delta file in {}.".format(os.path.join(sdir, d)), file=sys.stderr)
+                    continue
+                has_transfer_issue = os.stat(f).st_size > 0
+                
+                try:
                     f = glob.glob(os.path.join(sdir, d, "*.final.gff"))[0]
                 except:
                     print("No .final.gff in {}.".format(os.path.join(sdir, d)), file=sys.stderr)
                     continue
                 with open(f) as fin:
-                    trfcounts = countGFFFeatures(fin)
+                    trfcounts, trfgenecounts = countGFFFeatures(fin)
 
                 ref = d.replace(sample + ".", "")
                 print("SAMPLE=", sample, "REF=", ref)
-                row = [ref]
+                row = [ref, has_transfer_issue]
                 if not header:
                     header = ANN_REPORT_HEADER
                     #header = ["Reference"]
@@ -84,8 +105,13 @@ def main(args_in=sys.argv[1:]):
                     #header.extend(["Total[ref]", "Total[transferred]", "Total[%]"])
                     print(*header, sep="\t", file=ratt_out)
                      
+                row.append(len(refgenecounts.get(ref, set())))
+                row.append(len(trfgenecounts))
+                row.append(row[-1]/row[-2] if row[-2] else "NA")
+
                 row.extend([refcounts.get(ref, Counter())[feature] for feature in FEATURES])
                 row.extend([trfcounts[feature] for feature in FEATURES])
+               
                 rtotal, ttotal = 0, 0
                 for feature in FEATURES:
                     rcount = refcounts.get(ref, Counter())[feature]
