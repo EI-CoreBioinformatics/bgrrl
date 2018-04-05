e9ff113cd652c706d1389f658d4bb98be2fb457a
Christian Schudoma (cschu) <christian.schudoma@earlham.ac.uk>
2018-04-05 16:19:19 +0100

    Reactivated Enterobase packaging

--- bgrrl/bgrrl/zzz/bgrrl-fin.smk.py
+++ bgrrl/bgrrl/zzz/bgrrl-fin.smk.py
@@ -26,14 +26,60 @@ EB_ORGANISMS = config.get("enterobase_groups", list())  # ["Salmonella"]
 CWD = os.getcwd()
 
 
+print("IN_SNAKE:")
+print(config)
+print(EB_ORGANISMS)
+
 if EB_ORGANISMS:
 	rule all:
 		input: 
 			expand(join(OUTPUTDIR, "{organism}_ASSEMBLY_PKG_DONE"), organism=EB_ORGANISMS),
-		#	expand(join(OUTPUTDIR, "{organism}_READ_PKG_DONE"), organism=EB_ORGANISMS),
+			expand(join(OUTPUTDIR, "{organism}_READ_PKG_DONE"), organism=EB_ORGANISMS),
 		#	expand(join(OUTPUTDIR, "{organism}_RESULTS_PKG_DONE"), organism=EB_ORGANISMS),
 		#	expand(join(OUTPUTDIR, config["project_prefix"] + "_{organism}_multiqc_report.html"), organism=EB_ORGANISMS)
 		# TARGETS
+
+	rule fin_compile_assembly:
+		input:
+			eb_samples = join(INPUTDIR, "reports", "eb_{organism}_samples.txt")		
+		output:
+			done = join(OUTPUTDIR, "{organism}_ASSEMBLY_PKG_DONE")
+		params:
+			outdir = lambda wildcards: join(OUTPUTDIR, config["project_prefix"] + "_" + wildcards.organism + "_assemblies"),
+			prefix = config["project_prefix"]
+		shell:
+			"mkdir -p {params.outdir} &&" + \
+			" (for s in $(cat {input.eb_samples}); do" + \
+			" ln -s ../../Analysis/assembly/$s/$s.assembly.fasta {params.outdir}/$s.assembly.fasta;" + \
+			" done)" + \
+			" && cd " + OUTPUTDIR + \
+			" && tar chvzf $(basename {params.outdir}).tar.gz $(basename {params.outdir})" + \
+			" && cd " + CWD + \
+			" && md5sum {params.outdir}.tar.gz > {params.outdir}.tar.gz.md5" + \
+			" && touch {output.done}"
+
+	rule fin_compile_reads:
+		input:
+			eb_samples = join(INPUTDIR, "reports", "eb_{organism}_samples.txt"),
+			samplesheet = config["samplesheet"]
+		output:
+			done = join(OUTPUTDIR, "{organism}_READ_PKG_DONE")
+		params:
+			outdir = lambda wildcards: join(OUTPUTDIR, config["project_prefix"] + "_" + wildcards.organism + "_rawreads"),
+			prefix = config["project_prefix"]
+		shell:
+			"mkdir -p {params.outdir} &&" + \
+			" (for r in $(grep -F -f {input.eb_samples} {input.samplesheet} | cut -f 3 -d ,); do" + \
+			" ln -s $r {params.outdir}/$(basename $r);" + \
+			" r=$(dirname $r)/$(basename $r _R1.fastq.gz)_R2.fastq.gz;" + \
+			" ln -s $r {params.outdir}/$(basename $r); done)" + \
+			" && cd " + OUTPUTDIR + \
+			" && tar chvzf $(basename {params.outdir}).tar.gz $(basename {params.outdir})" + \
+			" && cd " + CWD + \
+			" && md5sum {params.outdir}.tar.gz > {params.outdir}.tar.gz.md5" + \
+			" && touch {output.done}"
+
+
 elif config["finalize_mode"] == "asm":
 	rule all:
 		input: 
@@ -125,45 +171,26 @@ elif config["finalize_mode"] == "ann":
                         md5sum $(basename {output.tarball}) > $(basename {output.tarball}).md5
 			"""
 
-#rule fin_compile_assembly:
-#	input:
-#		eb_samples = join(INPUTDIR, "reports", "{organism}_EB_samples.txt")		
-#	output:
-#		done = join(OUTPUTDIR, "{organism}_ASSEMBLY_PKG_DONE")
-#	params:
-#		outdir = lambda wildcards: join(OUTPUTDIR, config["project_prefix"] + "_" + wildcards.organism + "_assemblies"),
-#		prefix = config["project_prefix"]
-#	shell:
-#		"mkdir -p {params.outdir} &&" + \
-#		" (for s in $(cat {input.eb_samples}); do" + \
-#		" ln -s ../../Analysis/assembly/$s/$s.assembly.fasta {params.outdir}/$s.assembly.1k.fasta;" + \
-#		" done)" + \
-#		" && cd " + OUTPUTDIR + \
-#		" && tar chvzf $(basename {params.outdir}).tar.gz $(basename {params.outdir})" + \
-#		" && cd " + CWD + \
-#		" && md5sum {params.outdir}.tar.gz > {params.outdir}.tar.gz.md5" + \
-#		" && touch {output.done}"
-#
-#rule fin_compile_reads:
-#	input:
-#		eb_samples = join(INPUTDIR, "reports", "{organism}_EB_samples.txt"),
-#		samplesheet = config["samplesheet"]
-#	output:
-#		done = join(OUTPUTDIR, "{organism}_READ_PKG_DONE")
-#	params:
-#		outdir = lambda wildcards: join(OUTPUTDIR, config["project_prefix"] + "_" + wildcards.organism + "_rawreads"),
-#		prefix = config["project_prefix"]
-#	shell:
-#		"mkdir -p {params.outdir} &&" + \
-#		" (for r in $(grep -F -f {input.eb_samples} {input.samplesheet} | cut -f 3 -d ,); do" + \
-#		" ln -s $r {params.outdir}/$(basename $r);" + \
-#		" r=$(dirname $r)/$(basename $r _R1.fastq.gz)_R2.fastq.gz;" + \
-#		" ln -s $r {params.outdir}/$(basename $r); done)" + \
-#		" && cd " + OUTPUTDIR + \
-#		" && tar chvzf $(basename {params.outdir}).tar.gz $(basename {params.outdir})" + \
-#		" && cd " + CWD + \
-#		" && md5sum {params.outdir}.tar.gz > {params.outdir}.tar.gz.md5" + \
-#		" && touch {output.done}"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #
 #rule fin_compile_qa:
 #	input:
@@ -199,25 +226,3 @@ elif config["finalize_mode"] == "ann":
 #		" && cd " + CWD + \
 #		" && md5sum {params.outdir}.tar.gz > {params.outdir}.tar.gz.md5" + \
 #		" && touch {output.done}) &> {log}"
-#
-#rule fin_multiqc:
-#	input:
-#		join(OUTPUTDIR, "{organism}_RESULTS_PKG_DONE")
-#	output:
-#		join(OUTPUTDIR, config["project_prefix"] + "_{organism}_multiqc_report.html")
-#	params:	
-#		prefix = config["project_prefix"],
-#		load = loadPreCmd(config["load"]["multiqc"]),
-#		mqc_config = config["multiqc_config"],
-#		datadir = lambda wildcards: join(OUTPUTDIR, config["project_prefix"] + "_" + wildcards.organism + "_results"),
-#		outdir = OUTPUTDIR
-#	shell:
-#		"{params.load}" + TIME_CMD + \
-#		" multiqc -f -n {params.prefix}_{wildcards.organism}_multiqc_report -i {params.prefix} -z -c {params.mqc_config} -o {params.outdir} {params.datadir}"
-#
-#"""
-#	rule fin_table2xls:
-#		input:
-#			join()
-#"""	
-#
