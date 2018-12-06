111972453ab548c32463d43c873be7ed142e3345
Christian Schudoma (cschu) <christian.schudoma@earlham.ac.uk>
2018-12-06 18:44:17 +0000

    Cleaned up snakemake pipelines.

--- bgrrl/zzz/bgrrl-ann.smk.py
+++ bgrrl/zzz/bgrrl-ann.smk.py
@@ -8,56 +8,54 @@ from bgrrl.samplesheet import readSamplesheet
 from bgrrl import TIME_CMD
 from bgrrl.snakemake_helper import loadPreCmd
 
+DEBUG = config.get("debugmode", False)
+CWD = os.getcwd()
+
+# tools
+RATT_WRAPPER = join(config["etc"], "wrappers", "ratt_wrapper")
+
+# setup i/o
 OUTPUTDIR = config["out_dir"]
 ASSEMBLY_DIR = join(OUTPUTDIR, "assembly")
 ANNOTATION_DIR = join(OUTPUTDIR, "annotation")
 PROKKA_DIR = join(ANNOTATION_DIR, "prokka")
 RATT_DIR = join(ANNOTATION_DIR, "ratt")
 
-PROKKA_WRAPPER = join(config["etc"], "wrappers", "prokka_wrapper")
-RATT_WRAPPER = join(config["etc"], "wrappers", "ratt_wrapper")
-
-# INPUTFILES = dict(readSamplesheet(config["samplesheet"]))
-# with open("ann-inputfiles.txt", "w") as input_out:
-#     print(*INPUTFILES.values(), sep="\n", file=input_out)
 INPUTFILES = set(row[0] for row in csv.reader(open(config["samplesheet"]), delimiter=","))
 
+# generate target list
 TARGETS = list()
-REF_PREFIXES = dict()
 if config["run_prokka"]:
-	# TARGETS.extend(map(lambda s:join(PROKKA_DIR, s, s + ".log"), INPUTFILES))
 	TARGETS.extend(map(lambda s:join(PROKKA_DIR, s, s + ".prokka.gff"), INPUTFILES))
 	TARGETS.extend(map(lambda s:join(PROKKA_DIR, s, s + ".ffn.16S"), INPUTFILES))
 	
-
+REF_PREFIXES = dict()
 if config["run_ratt"]:
 	for d in next(os.walk(config["ratt_reference"]))[1]:     
-		# TARGETS.extend(map(lambda s:join(RATT_DIR, s, d, "{}_{}.done".format(s, d)), INPUTFILES))
 		TARGETS.extend(map(lambda s:join(RATT_DIR, s, d, "{}_{}.final.gff".format(s, d)), INPUTFILES))
-		REF_PREFIXES[d] = list(f.strip(".embl").split(".")[-1] for f in next(os.walk(join(config["ratt_reference"], d)))[2] if f.endswith(".embl"))
-		#for f in next(os.walk(join(config["ratt_reference"], d)))[2]:
-		#	if f.endswith(".embl"):
-		#		REF_PREFIXES.append(basename(f))
-		#		TARGETS.append(join(config["ratt_reference"], d, "gff", f.replace(".embl", ".gff")))
 		TARGETS.append(join(config["ratt_reference"], d, "gff", d + ".gff"))
+		REF_PREFIXES[d] = list(f.strip(".embl").split(".")[-1] for f in next(os.walk(join(config["ratt_reference"], d)))[2] if f.endswith(".embl"))
 
-CWD = os.getcwd()
 
-print("CONFIG")
-print(config)
+if DEBUG:
+	print("CONFIG")
+	print(config)
 
-with open("ann-targets.txt", "w") as targets_out:
-	print(*TARGETS, sep="\n", file=targets_out)
+	with open("ann-targets.txt", "w") as targets_out:
+		print(*TARGETS, sep="\n", file=targets_out)
 
+# helpers
 def get_ref_index(wc):
 	return REF_PREFIXES.get(wc.ref_ann)
-
 
 def get_ref(wc):
 	if len(REF_PREFIXES[wc.ref_ann]) > 1:
 		return [join(config["ratt_reference"], wc.ref_ann, "gff", "{}.{}.parts_gff".format(wc.ref_ann, index)) for index in REF_PREFIXES[wc.ref_ann]]
 	else:
 		return [join(config["ratt_reference"], wc.ref_ann, "gff", "{}.parts_gff".format(wc.ref_ann))]
+
+
+### RULES ###
 
 localrules: all, ann_prokka_gffconvert, ann_prokka_16S
 
@@ -66,6 +64,8 @@ rule all:
 
 if config["run_prokka"]:
 	rule ann_prokka:
+		message:
+			"Running de novo gene/functional annotation with prokka (incl. barrnap)..."
 		input:
 			contigs = join(ASSEMBLY_DIR, "{sample}", "{sample}.assembly.fasta")
 		output:
@@ -86,26 +86,10 @@ if config["run_prokka"]:
 			"set +u && source activate prokka_env" + \
 			" && prokka_wrapper {input.contigs} --prefix {params.prefix} --outdir {params.outdir} --seq-centre {params.centre} --force --threads {threads}" + \
 			" &> {log}"
-			 
-
-			# ap = argparse.ArgumentParser()
-			#ap.add_argument("contigs", type=str)
-			#ap.add_argument("--outdir", type=str)
-			#ap.add_argument("--prefix", type=str)
-			#ap.add_argument("--seq-centre", type=str, default="EI")
-			#ap.add_argument("--force")
-			#ap.add_argument("--threads", type=int, default=8)
-
-			#"cd {params.outdir}" + \
-			#" && echo $(pwd)" + \
-			#" && cp -v " + join(config["cwd"], "{input.contigs}") + " prokka_contigs.fasta" + \
-			#" && ls -l" + \
-			#" && {params.load} (" + TIME_CMD + \				
-			#" prokka --cpus {threads} --outdir . --prefix {params.prefix} --centre {params.centre} prokka_contigs.fasta --force" + \
-			#" && rm prokka_contigs.fasta" + \
-			#" && cd " + CWD + ") &> {log}"
 
 	rule ann_prokka_gffconvert:
+		message:
+			"Renaming contig/scaffold names in prokka-generated gff..." # !TODO might need to do that with .gbk, too?
 		input:
 			gff = join(PROKKA_DIR, "{sample}", "{sample}.gff")
 		output:
@@ -117,6 +101,8 @@ if config["run_prokka"]:
 			" {{ if (NF > 1) print $0; }}' {input.gff} > {output.gff}"
 
 	rule ann_prokka_16S:
+		message:
+			"Extracting 16S rRNA sequences from barrnap-annotation..."
 		input:
 			ffn = join(PROKKA_DIR, "{sample}", "{sample}.ffn")
 		output:
@@ -130,51 +116,50 @@ if config["run_prokka"]:
 
 
 if config["run_ratt"]:
+	rule ann_ratt_prepref:
+		message:
+			"Preprocessing reference annotations for ratt-annotation transfer..."
+		input:
+			embl_ref = join(config["ratt_reference"], "{ref_ann}", "{prefix}.embl")
+		output:
+			join(config["ratt_reference"], "{ref_ann}", "gff", "{prefix}.parts_gff")
+		# log:
+		#	join(config["cwd"], ANNOTATION_DIR, "log", "{ref_ann}.ann_ratt_prepref.log")
+		params:
+			load = loadPreCmd(config["load"]["ratt"]),
+			refdir = join(config["cwd"], config["ratt_reference"], "{ref_ann}")				
+		threads:
+			1
+		shell:
+			"{params.load}" + \
+			" cd {params.refdir}" + \
+			" && seqret -sequence {input.embl_ref} -outseq $(basename {input.embl_ref} .embl).$(basename {input.embl_ref} .embl | sed 's/\./_x_/g').gff -offormat gff -feature" + \
+			" && rm $(basename {input.embl_ref} .embl).$(basename {input.embl_ref} .embl | sed 's/\./_x_/g').gff" + \
+			" && mkdir -p gff && mv $(basename {input.embl_ref} .embl | sed 's/\./_x_/g').gff gff/$(basename {input.embl_ref} .embl | sed 's/_x_/./g').parts_gff && cd " + CWD 
 
-	if True:
-
-		rule ann_ratt_prepref:
-			input:
-				embl_ref = join(config["ratt_reference"], "{ref_ann}", "{prefix}.embl")
-			output:
-				join(config["ratt_reference"], "{ref_ann}", "gff", "{prefix}.parts_gff")
-			# log:
-			#	join(config["cwd"], ANNOTATION_DIR, "log", "{ref_ann}.ann_ratt_prepref.log")
-			params:
-				load = loadPreCmd(config["load"]["ratt"]),
-				refdir = join(config["cwd"], config["ratt_reference"], "{ref_ann}")				
-			threads:
-				1
-			shell:
-				"{params.load}" + \
-				" cd {params.refdir}" + \
-				" && seqret -sequence {input.embl_ref} -outseq $(basename {input.embl_ref} .embl).$(basename {input.embl_ref} .embl | sed 's/\./_x_/g').gff -offormat gff -feature" + \
-				" && rm $(basename {input.embl_ref} .embl).$(basename {input.embl_ref} .embl | sed 's/\./_x_/g').gff" + \
-				" && mkdir -p gff && mv $(basename {input.embl_ref} .embl | sed 's/\./_x_/g').gff gff/$(basename {input.embl_ref} .embl | sed 's/_x_/./g').parts_gff && cd " + CWD 
-
-
-		rule ann_ratt_mergegff:
-			input:
-				# gff = expand(join(config["ratt_reference"], "{ref_ann}", "gff", "{ref_ann}.{index}.parts_gff"), ref_ann=next(os.walk(config["ratt_reference"]))[1], index=get_ref_index)
-				gff = get_ref
-			output:
-				join(config["ratt_reference"], "{ref_ann}", "gff", "{ref_ann}.gff")
-			log:
-				join(config["cwd"], ANNOTATION_DIR, "log", "{ref_ann}.ann_ratt_mergegff.log")
-			params:
-				gffdir = join(config["cwd"], config["ratt_reference"], "{ref_ann}", "gff")
-			threads:
-				1
-			shell:
-				# "cd {params.gffdir}" + \
-				"cat {input.gff} > {output}"
+	rule ann_ratt_mergegff:
+		message:
+			"Merging single-gene .gffs from ratt-annotation transfer..."
+		input:
+			gff = get_ref
+		output:
+			join(config["ratt_reference"], "{ref_ann}", "gff", "{ref_ann}.gff")
+		log:
+			join(config["cwd"], ANNOTATION_DIR, "log", "{ref_ann}.ann_ratt_mergegff.log")
+		params:
+			gffdir = join(config["cwd"], config["ratt_reference"], "{ref_ann}", "gff")
+		threads:
+			1
+		shell:
+			"cat {input.gff} > {output}"
 
 	rule ann_ratt:
+		message:
+			"Running annotation-transfer with ratt..."
 		input:
 			contigs = join(ASSEMBLY_DIR, "{sample}", "{sample}.assembly.fasta"),
 			reference = join(config["ratt_reference"], "{ref_ann}")
 		output:
-			# done = join(RATT_DIR, "{sample}", "{ref_ann}", "{sample}_{ref_ann}.done")
 			done = join(RATT_DIR, "{sample}", "{ref_ann}", "{sample}_{ref_ann}.final.gff")
 		log:
 			join(config["cwd"], ANNOTATION_DIR, "log", "{sample}_{ref_ann}.ann_ratt.log")
@@ -187,8 +172,3 @@ if config["run_ratt"]:
 		shell:
 			RATT_WRAPPER + " {params.outdir} " + join(config["cwd"], "{input.contigs}") + \
 			" {input.reference} {params.done} &> {log}"
-			# " touch " + join(config["cwd"], "{output.done}") + ")" 
-			# " && extractfeat -sequence *.final.embl -outseq {params.prefix}.fasta -type CDS" + \
-			# " && transeq -sequence {params.prefix}.fasta {params.prefix}.pep.fasta" + \
-			# " && mv {params.prefix}.fasta {params.prefix}.ffn" + \
-			# " && mv {params.prefix}.pep.fasta {params.prefix}.faa" + \
