b1607d6ebbdb686afb2148ed0a673cf6ffb2fc9f
Christian Schudoma (cschu) <christian.schudoma@earlham.ac.uk>
2018-12-07 15:23:52 +0000

    Cosmetics.

--- bgrrl/__init__.py
+++ bgrrl/__init__.py
@@ -1,7 +1,7 @@
 import pkg_resources
 
 __title__ = "bgrr|"
-__author__ = "Christian Schudoma"
+__author__ = "Christian Schudoma (cschu)"
 __email__ = "christian.schudoma@earlham.ac.uk"
 __license__ = "MIT"
 __copyright__ = "Copyright 2017-2018 Earlham Institute"
