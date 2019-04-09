0f43f0a8c2dd3c08eef7ac907c2e9f0c961521be
Christian Schudoma (cschu) <christian.schudoma@earlham.ac.uk>
2019-04-09 16:01:33 +0100

    version 0.6.1->0.6.2

--- build.sh
+++ build.sh
@@ -5,3 +5,5 @@ version=$1
 # rm dist/*whl
 python setup.py bdist_wheel
 pip install --prefix=/ei/software/testing/bgrrl/${version}/x86_64 -U dist/bgrrl-${version}-*.whl
+
+cp /ei/software/testing/bin/bgrrl-0.6.1 /ei/software/testing/bin/bgrrl-${version}
