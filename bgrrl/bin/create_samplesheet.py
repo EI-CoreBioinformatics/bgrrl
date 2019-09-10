3bb7f8e7a6af5a11191e05f4e190fb2d162512af
Christian Schudoma (cschu) <christian.schudoma@earlham.ac.uk>
2019-09-11 00:41:17 +0100

    Added --single-cell option

--- bgrrl/bin/create_samplesheet.py
+++ bgrrl/bin/create_samplesheet.py
@@ -10,6 +10,7 @@ def main(argv=sys.argv[1:]):
     ap = argparse.ArgumentParser()
     ap.add_argument("readpath", type=str)
     ap.add_argument("--lane", "-l", type=str, default="")
+	ap.add_argument("--single-cell", action="store_true")
     args = ap.parse_args()
 
     lane = ("_" + args.lane) if args.lane else ""
@@ -17,7 +18,7 @@ def main(argv=sys.argv[1:]):
     with open("samplesheet.csv", "w") as samplesheet:
         for f in sorted(glob.glob(os.path.join(args.readpath, "*{}_R1.fastq.gz".format(lane)))):
             s = os.path.basename(f).replace("{}_R1.fastq.gz".format(lane), "")
-            print(s, s, f, f.replace("R1.fastq.gz", "R2.fastq.gz"), ",,,,,", sep=",", file=samplesheet)
+            print(s, s, f, f.replace("R1.fastq.gz", "R2.fastq.gz") if not args.single_cell else "", ",,,,,", sep=",", file=samplesheet)
     return True
 
 
