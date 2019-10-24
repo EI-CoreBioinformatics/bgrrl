89eb07805478b21b7d4fb3476bead18162856aa7
Christian Schudoma (cschu) <christian.schudoma@earlham.ac.uk>
2019-10-24 20:27:59 +0100

    cleanup.

--- qaa/qaa_environment.py
+++ qaa/qaa_environment.py
@@ -2,15 +2,11 @@ from os.path import join
 
 
 
-class QAA_Environment(object):
+class QaaEnvironment(object):
 	def __init__(self, config):		
-		self.cwd = config.get("cwd", ".")
 
 		# output_dir = Analysis-dir
-		self.output_dir = config.get("out_dir", ".")
-		# ensure to use absolute paths (I think because of busco?)
-		if not self.output_dir.startswith("/"):
-			self.output_dir = join(self.cwd, self.output_dir)
+		self.output_dir = config.get("output_dir", ".")
 
 		self.qa_dir = self.qaa_dir = join(self.output_dir, "qaa", "survey" if config["survey_assembly"] else "asm")
 		self.qc_dir = join(self.output_dir, "qc")
@@ -21,7 +17,7 @@ class QAA_Environment(object):
 		self.busco_geno_dir = join(self.busco_dir, "geno")
 		self.busco_tran_dir = join(self.busco_dir, "tran")
 		self.busco_prot_dir = join(self.busco_dir, "prot")
-		self.busco_init_dir = join(config["etc"], "busco", "busco_init_dir")
+		self.busco_init_dir = join(config["etc_dir"], "busco", "busco_init_dir")
 		self.busco_data_dir = config["resources"]["busco_databases"]
 		
 		# tool dirs
